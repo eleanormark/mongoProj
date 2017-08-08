ced25fb745a29eab4aa5dc07cf4a3961c1458a90
Eleanor Mark <eleanormark@gmail.com>
2017-08-07 22:28:39 -0700

    added validation tests

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({name: "Joe"});
+        joe = new User({name: "Joe", postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -33,7 +33,6 @@ describe('Updating records', () => {
         assertName(
             User.update({name: 'Joe'}, {name: "Alex"}),
             done);
-        
     });
 
     it('A model class can update one recored', (done) => {
@@ -46,6 +45,15 @@ describe('Updating records', () => {
         assertName(
             User.findByIdAndUpdate(joe._id, {name: "Alex"}),
             done);
+    });
+         
+    it('A user can have thier postCount incremented by 1', (done) => {
+        User.update({name: 'Joe'}, {$inc: {postCount: 1}})
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            })
     });
 
 });
