381fdb982968c842efefc82f1e978038fc9ef53d
Eleanor Mark <eleanormark@gmail.com>
2017-08-11 12:15:42 -0700

    added middleware test

---
+++ test/middleware_test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/user');
+const BlogPost = require('../src/blogPost');
+
+describe('Middleware', () => {
+
+    let joe, blogPost;
+
+    beforeEach((done) => {
+        joe = new User({name: 'Joe'});
+        blogPost = new BlogPost({ title: 'JS is Great', content: "Lorem Ipsum" });
+
+        //Association setup
+        joe.blogPosts.push(blogPost);
+
+        Promise.all([joe.save(), blogPost.save()])
+            .then(() => done());
+    });
+
+    it('users clean up dangling blogposts on remove', (done) => {
+        joe.remove()
+            .then(() => BlogPost.count()
+            .then((countResult) => {
+                assert(countResult === 0);
+                done();
+            }))
+    });
+
+})
