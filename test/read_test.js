11b74e4e8f02fd0776683c65a9792f794235a5e7
Eleanor Mark <eleanormark@gmail.com>
2017-08-11 12:55:27 -0700

    added pagination test

--- test/read_test.js
+++ test/read_test.js
@@ -2,12 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-    let joe;
+    let joe, alex, ann, zach;
 
     beforeEach((done) => {
+        alex = new User({name: 'Alex'});
+        ann = new User({name: 'Ann'});
         joe = new User({name: 'Joe'});
-        joe.save().
-            then(() => { done(); });
+        zach = new User({name: 'Zach'});
+
+        Promise.all([alex.save(), ann.save(), joe.save(), zach.save()])
+            .then(() => done())
     });
 
     it('finds all users with a name of joe', (done) => {
@@ -23,8 +27,20 @@ describe('Reading users out of the database', () => {
             .then((user) => {
                 assert(user.name === "Joe");
                 done();
-
             })
+    });
+
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+        .sort({name: 1})
+        .skip(1)
+        .limit(2)
+        .then((users) => {
+            assert(users.length === 2);
+            assert(users[0].name === 'Ann');
+            assert(users[1].name === 'Joe');
+            done();
+        });
 
     });
 
